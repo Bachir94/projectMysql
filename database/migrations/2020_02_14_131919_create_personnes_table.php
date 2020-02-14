d68c4d25aa70ff73044fe6265c5c8ec48306d762
Nom <Email>
2020-02-14 14:48:46 +0100

    first commit

---
+++ database/migrations/2020_02_14_131919_create_personnes_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePersonnesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('personnes', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->char('nom', 110);
+            $table->char('prenom', 80);
+            $table->char('Email', 255);
+            $table->dateTime('Date de naissance');
+            $table->integer('numéro de telephonne');
+            $table->boolean('inscrit');
+            $table->text('description');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('personnes');
+    }
+}
